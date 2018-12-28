1ea75045e89cc3dab004850bd067b68035f5e9a9
buliba <buliba@bicitech.cn>
2018-12-28 17:56:51 +0800

    抽奖功能设计-准备工作

--- src/main/java/com/yyq/wedding/wechat/HomeController.java
+++ src/main/java/com/yyq/wedding/wechat/HomeController.java
@@ -23,6 +23,8 @@ public class HomeController {
 
     @Value("${token}")
     private String token;
+    @Value("${luckDrawTime}")
+    private String luckDrawTime;//指定抽奖时间
     private static String text;
     private static String sendUsername;
     private Logger logger = LoggerFactory.getLogger(getClass().getName());//日志
@@ -31,7 +33,7 @@ public class HomeController {
     @ResponseBody
     public void liaotian(Model model, HttpServletRequest request, HttpServletResponse response) {
         response.setCharacterEncoding("UTF-8");
-        logger.info("验证TOKEN"+token);
+        logger.info("验证TOKEN" + token);
         boolean isGet = request.getMethod().toLowerCase().equals("get");
         if (isGet) {
             String signature = request.getParameter("signature");
@@ -123,10 +125,11 @@ public class HomeController {
         String msgType = inputMsg.getMsgType();
         // 根据消息类型获取对应的消息内容
         sendUsername = inputMsg.getFromUserName();
-        logger.info("发送信息内容============="+inputMsg.getContent());
+        logger.info("用户id=============" + sendUsername);
+        logger.info("发送信息内容=============" + inputMsg.getContent());
         text = inputMsg.getContent();
-        if(text.contains("傻逼")||text.contains("白痴")){
-            text="新婚快乐";
+        if (text.contains("傻逼") || text.contains("白痴")) {
+            text = "新婚快乐";
             StringBuffer str = new StringBuffer();
             str.append("<xml>");
             str.append("<ToUserName><![CDATA[" + custermname + "]]></ToUserName>");
@@ -145,10 +148,10 @@ public class HomeController {
         str.append("<FromUserName><![CDATA[" + servername + "]]></FromUserName>");
         str.append("<CreateTime>" + returnTime + "</CreateTime>");
         str.append("<MsgType><![CDATA[" + msgType + "]]></MsgType>");
-        if (text.contains("/:") || text.contains("收到不支持的消息类型，暂无法显示")){
+        if (text.contains("/:") || text.contains("收到不支持的消息类型，暂无法显示")) {
             str.append("<Content><![CDATA[暂时不支持发送表情弹幕]]></Content>");
-            text="新婚快乐";
-        }else {
+            text = "新婚快乐";
+        } else {
             str.append("<Content><![CDATA[弹幕\"" + text + "\"发送成功]]></Content>");
         }
         str.append("</xml>");
